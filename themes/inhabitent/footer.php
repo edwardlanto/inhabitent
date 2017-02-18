cb2e45c6d84ab5724d0b90c326cc4e564287afbf
Edward <you@email.com>
2017-02-18 13:13:24 -0800

    positioned entry-title and entry-meta in journal

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -7,9 +7,19 @@
 
 ?>
 
-			</div><!-- #content -->
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
+			<div class = "contact-container">
+				<h3 class = "contact-footer">Contact Info</h3>
+				<a class =  "white-contact-text">info@inhabitent.com</a>
+				<a class = "white-contact-text">778-456-7891</a>
+			</span>
+			</div>
+			<div class = "business-container">
+				<h3 class = "business-footer">Business Hours</h3>
+				<p class ="date-footer">Monday-Friday:9am to 5pm;</p>
+				<p class ="date-footer">Sunday:Closed</p>
+			</div>
 				<div class="site-info">
 					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
 				</div><!-- .site-info -->
