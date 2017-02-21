e9287eafe55edf87773aeb9d9d5447d092741269
Edward <you@email.com>
2017-02-21 13:02:42 -0800

    added adventure pic text

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -12,6 +12,46 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 
 		<main id="main" class="site-main" role="main">
+			<div class = "front-hero-image">
+  <div class = "front-logo-image"></div>
+</div>
+
+<h2 class = "home-content-heading">Shop Stuff</h2>
+<section></section>
+<h2 class = "home-content-heading">Inhabitent Journal</h2>
+<section></section>
+<h2 class = "home-content-heading">Latest Adventures</h2>
+<section class = "adventure-wrapper">
+  <div class = "adventures-pic-container">
+     <div class = "canoe-container">
+   
+         <div class = "canoe-large-pic adventure-pic">
+			 <h3 class ="adventure-text"><a>Getting Back to Nature in a Canoe</a></h3>
+     </div>
+     <div class ="beach-container">
+          <div class = "beach-medium-pic adventure-pic">
+			  <h3 class ="adventure-text"><a>A Night with Friends at the Beach</a></h3>
+		  </div>
+     </div>
+     <div class ="mountain-container">
+
+         <div class ="mountain-small-pic adventure-pic">
+			 <h3 class ="adventure-text"><a>Taking in the View at Big Mountain</a></h3>
+		 </div>
+     </div>
+     <div class ="star-container">
+         <div class ="star-small-pic adventure-pic">
+			 <h3 class ="adventure-text"><a>Star-Gazing at the Night Sky</a></h3>
+		 </div>
+     </div>
+
+  </div>
+</section>
+   
+
+       
+
+
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
